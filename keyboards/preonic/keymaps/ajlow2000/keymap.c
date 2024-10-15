0f9e469579079925635936dc8ea1c8a0fead814d
ajlow2000 <ajlow2000@gmail.com>
2024-10-15 21:14:13 +0000

    feat: prog save

--- keyboards/preonic/keymaps/ajlow2000/keymap.c
+++ keyboards/preonic/keymaps/ajlow2000/keymap.c
@@ -40,10 +40,11 @@ tap_dance_action_t tap_dance_actions[] = {
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
 #define CTL_ENT MT(MOD_RCTL, KC_ENT)
 #define ALT_ENT MT(MOD_LALT, KC_ENT)
+#define SFT_SLH MT(MOD_RSFT, KC_SLSH)
 
 /* Layer stuff */
 #define DF_Q DF(_QWERTY)
-#define DF_R DF(_RECURVA)
+#define DF_G DF(_GRAPHITE)
 #define L_NAV TT(_NAV)
 #define L_SYM TT(_SYM)
 #define L_NUM LT(_NUM, KC_SPC)
@@ -55,21 +56,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_TAB,
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_SLH,
+        XXXXXXX, XXXXXXX, KC_LGUI, ALT_ENT, KC_SPC,  L_SYM,   L_NAV,   L_NUM,   KC_BSPC, KC_DEL,  XXXXXXX, XXXXXXX
+    ),
+
+    // https://github.com/rdavison/graphite-layout
+    [_GRAPHITE] = LAYOUT_preonic_grid(
+        TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
+        KC_TAB,  KC_B,    KC_L,    KC_D,    KC_W,    KC_Z,    KC_QUOT, KC_F,    KC_O,    KC_U,    KC_J,    KC_TAB,
+        CTL_ESC, KC_N,    KC_R,    KC_T,    KC_S,    KC_G,    KC_Y,    KC_H,    KC_A,    KC_E,    KC_I,    CTL_ENT,
+        KC_LSFT, KC_Q,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_P,    KC_DOT,  KC_MINS, KC_COMM, SFT_SLH,
         XXXXXXX, XXXXXXX, KC_LGUI, ALT_ENT, KC_SPC,  L_SYM,   L_NAV,   L_NUM,   KC_BSPC, KC_DEL,  XXXXXXX, XXXXXXX
     ),
 
     [_SYM] = LAYOUT_preonic_grid(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, KC_BSLS, KC_QUOT, KC_DQUO, KC_PERC, KC_QUES, KC_EXLM, KC_RBRC, KC_LBRC, KC_GRV,  KC_SCLN, _______,
-        _______, KC_CIRC, KC_EQL,  KC_UNDS, KC_DLR,  KC_ASTR, KC_AT,   KC_RCBR, KC_LCBR, KC_TILD, KC_COLN, _______,
-        _______, KC_LT,   KC_PIPE, KC_MINS, KC_GT,   KC_SLSH, KC_PLUS, KC_RPRN, KC_LPRN, KC_HASH, KC_AMPR, _______,
+        _______, KC_BSLS, KC_QUOT, KC_DQUO, KC_PERC, KC_QUES, KC_EXLM, KC_RBRC, KC_LBRC, KC_GRV,  KC_SCLN,  ______,
+        _______, KC_CIRC, KC_EQL,  KC_UNDS, KC_DLR,  KC_ASTR, KC_AT,   KC_RCBR, KC_LCBR, KC_TILD, KC_COLN,  ______,
+        _______, KC_LT,   KC_PIPE, KC_MINS, KC_GT,   KC_SLSH, KC_PLUS, KC_RPRN, KC_LPRN, KC_HASH, KC_AMPR,  ______,
         XXXXXXX, _______, _______, _______, _______, TG(_SYM), _______, _______, _______, _______, _______, XXXXXXX
     ),
 
     [_NAV] = LAYOUT_preonic_grid(
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        _______, QK_BOOT, XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, QK_BOOT, _______,
+        _______, QK_BOOT, XXXXXXX, KC_MS_U, XXXXXXX, DF_G,    KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, QK_BOOT, _______,
         _______, DF_Q,    KC_MS_L, KC_MS_D, KC_MS_R, GAME,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, G(KC_L), _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, _______,
         XXXXXXX, _______, _______, _______, KC_BTN1, KC_BTN2, TG(_NAV),_______, _______, _______, _______, XXXXXXX
@@ -90,15 +100,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F4,
         XXXXXXX, XXXXXXX, KC_LCTL, KC_LALT, KC_SPC,  L_SYM,   L_NAV,   L_NUM,   KC_BSPC, KC_F1,   KC_F2,   XXXXXXX
     ),
-
-    // // https://github.com/GalileoBlues/Recurva
-    // [_RECURVA] = LAYOUT_preonic_grid(
-    //     TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
-    //     KC_TAB,  KC_F,    KC_R,    KC_D,    KC_P,    KC_V,    KC_Q,    KC_J,    KC_U,    KC_O,    KC_Y,    KC_TAB,
-    //     CTL_ESC, KC_S,    KC_N,    KC_T,    KC_C,    KC_B,    KC_DOT,  KC_H,    KC_E,    KC_A,    KC_I,    CTL_ENT,
-    //     KC_LSFT, KC_Z,    KC_X,    KC_K,    KC_G,    KC_W,    KC_M,    KC_L,    KC_SCLN, KC_QUOT, KC_COMM, KC_RSFT,
-    //     XXXXXXX, XXXXXXX, KC_LGUI, ALT_ENT, KC_SPC,  L_SYM,   L_NAV,   L_NUM,   KC_BSPC, KC_DEL,  XXXXXXX, XXXXXXX
-    // ),
 
     // [PLACEHOLDER] = LAYOUT_preonic_grid(
     //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
