f14d40fde93ba503e9094acacd3663f376494416
ajlow2000 <ajlow2000@gmail.com>
2024-12-02 19:49:27 +0000

    feat: default graphite and fix ampr missing

--- keyboards/splitkb/elora/keymaps/ajlow2000/keymap.c
+++ keyboards/splitkb/elora/keymaps/ajlow2000/keymap.c
@@ -17,8 +17,8 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY,
     _GRAPHITE,
+    _QWERTY,
     _GAME,
     _SYM,
     _NAV,
@@ -53,22 +53,21 @@ tap_dance_action_t tap_dance_actions[] = {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_GRAPHITE] = LAYOUT_myr(
+        TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                XXXXXXX, XXXXXXX,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
+        KC_TAB , KC_B,    KC_L,    KC_D,    KC_W,    KC_Z,                XXXXXXX, XXXXXXX,          KC_QUOT, KC_F,    KC_O,    KC_U,    KC_J,    KC_TAB,
+        CTL_ESC, KC_N,    KC_R,    KC_T,    KC_S,    KC_G,                XXXXXXX, XXXXXXX,          KC_Y,    KC_H,    KC_A,    KC_E,    KC_I,    CTL_ENT,
+        KC_LSFT, KC_Q,    KC_X,    KC_M,    KC_C,    KC_V,    C(KC_LSFT), KC_F5,   KC_F11,  KC_DEL,  KC_K,    KC_P,    KC_DOT,  KC_MINS, KC_COMM, SFT_SLH,
+                                   XXXXXXX, ALT_ENT, KC_SPC,  L_SYM,      KC_LGUI, KC_TAB,  L_NAV,   NUM_SPC, KC_BSPC, XXXXXXX,
+
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX
+    ),
 
     [_QWERTY] = LAYOUT_myr(
         TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                XXXXXXX, XXXXXXX,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
         KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                XXXXXXX, XXXXXXX,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_TAB,
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                XXXXXXX, XXXXXXX,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    C(KC_LSFT), KC_F5,   KC_F11,  KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_SLH,
-                                   XXXXXXX, ALT_ENT, KC_SPC,  L_SYM,      KC_LGUI, KC_TAB,  L_NAV,   NUM_SPC, KC_BSPC, XXXXXXX,
-
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX
-    ),
-
-    [_GRAPHITE] = LAYOUT_myr(
-        TD_CV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                XXXXXXX, XXXXXXX,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    G(KC_L),
-        KC_TAB , KC_B,    KC_L,    KC_D,    KC_W,    KC_Z,                XXXXXXX, XXXXXXX,          KC_QUOT, KC_F,    KC_O,    KC_U,    KC_J,    KC_TAB,
-        CTL_ESC, KC_N,    KC_R,    KC_T,    KC_S,    KC_G,                XXXXXXX, XXXXXXX,          KC_Y,    KC_H,    KC_A,    KC_E,    KC_I,    CTL_ENT,
-        KC_LSFT, KC_Q,    KC_X,    KC_M,    KC_C,    KC_V,    C(KC_LSFT), KC_F5,   KC_F11,  KC_DEL,  KC_K,    KC_P,    KC_DOT,  KC_MINS, KC_COMM, SFT_SLH,
                                    XXXXXXX, ALT_ENT, KC_SPC,  L_SYM,      KC_LGUI, KC_TAB,  L_NAV,   NUM_SPC, KC_BSPC, XXXXXXX,
 
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX
@@ -88,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            XXXXXXX, XXXXXXX,          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         _______, KC_BSLS, KC_QUOT, KC_DQUO, KC_PERC, KC_QUES,          XXXXXXX, XXXXXXX,          KC_EXLM, KC_RBRC, KC_LBRC, KC_GRV,  KC_COLN, _______,
         _______, KC_CIRC, KC_EQL,  KC_UNDS, KC_DLR,  KC_ASTR,          XXXXXXX, XXXXXXX,          KC_AT,   KC_RCBR, KC_LCBR, KC_TILD, KC_SCLN, _______,
-        _______, KC_LT,   KC_PIPE, KC_MINS, KC_GT,   KC_SLSH, _______, _______, _______, _______, KC_PLUS, KC_RPRN, KC_LPRN, KC_HASH, KC_DOT,  _______,
+        _______, KC_LT,   KC_PIPE, KC_MINS, KC_GT,   KC_SLSH, _______, _______, _______, _______, KC_PLUS, KC_RPRN, KC_LPRN, KC_HASH, KC_AMPR, _______,
                                    XXXXXXX, _______, _______, TG(_SYM),_______, KC_COMM, KC_DOT,  _______, KC_DEL,  XXXXXXX,
 
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX
